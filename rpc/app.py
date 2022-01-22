c1aadde88505e3aa0616e3ab3f2e9084ee0bba07
hm <hm.tuomaala@gmail.com>
2022-01-22 18:22:00 +0200

    Add all games to games page

--- rpc/app.py
+++ rpc/app.py
@@ -4,7 +4,7 @@ import json
 import os
 import math
 import urllib.parse
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
@@ -113,14 +113,11 @@ def background_update_db():
 
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    api = "https://" + API_BASE + "/rps/history"
-    d = requests.get(api).json()
-    d["data"] = sorted(d["data"], key=lambda x: -x['t'])
-    return json.dumps(d["data"])
+    return render_template("index.html")
 
-@app.route('/update')
+@app.route("/update")
 def update():
     update_db_from_history()
     return "DATABASE UPDATED"
@@ -140,7 +137,6 @@ def players():
 
 @app.route("/players/<name>")
 def player(name):
-
     page = request.args.get("page", 1, type=int)
 
     data = {}
@@ -230,8 +226,10 @@ def live():
 
 @app.route("/games")
 def games():
+    page = request.args.get("page", 1, type=int)
+
     data = {}
-    data1 = query_db(
+    stats = query_db(
         """ select q1.player || ' (' || q1.games || ')' as most_games,
             q2.player || ' (' || q2.wins || ')' as most_wins,
             q3.player || ' (' || printf('%.5f',q3.ratio) || ')' as best_ratio,
@@ -293,83 +291,33 @@ def games():
             limit 1) q5 on q1.j = q5.j """
     )
 
-    data2 = query_db(
-        """ select q1.player || ' (' || q1.games || ')' as least_games,
-            q2.player || ' (' || q2.lost || ')' as most_lost,
-            q3.player || ' (' || printf('%.5f',q3.ratio) || ')' as worst_ratio,
-            q4.hand || ' (' || q4.total || ')' as lph,
-            q5.hand || ' (' || q5.total || ')' as mdh
-            from (
-            select player, count(*) as games, 1 as j from (
-            select player_a as player from games
-            union all
-            select player_b as player from games)
-            group by player
-            order by count(*) asc
-            limit 1) q1
-            join (
-            select player, count(*) as lost, 1 as j from (
-            select player_a as player from games
-            where winner = player_b
-            union all
-            select player_b as player from games
-            where winner = player_a)
-            group by player
-            order by count(*) desc
-            limit 1) q2 on q1.j = q2.j
-            join (
-            select w.player, wins, games, (wins * 1.0 / games) as ratio, 1 as j from (
-            select player, count(*) as wins from (
-            select player_a as player from games
-            where winner = player_a
-            union all
-            select player_b as player from games
-            where winner = player_b)
-            group by player
-            order by count(*) desc) w join (
-            select player, count(*) as games from (
-            select player_a as player from games
-            union all
-            select player_b as player from games)
-            group by player
-            order by count(*) desc) g on w.player = g.player
-            order by 4 asc
-            limit 1) q3 on q1.j = q3.j
-            join (
-            select hand, count(*) as total, 1 as j from (
-            select a_hand as hand from games
-            union all
-            select b_hand as hand from games)
-            group by hand
-            order by 2 asc
-            limit 1) q4 on q1.j = q4.j
-            join (
-            select hand, count(*) as total, 1 as j from (
-            select a_hand as hand from games
-            where winner = player_b
-            union all
-            select b_hand as hand from games
-            where winner = player_a)
-            group by hand
-            order by count(*) desc
-            limit 1) q5 on q1.j = q5.j """
+    games = query_db(
+        """ SELECT id,
+                   DATETIME(ROUND(time / 1000), 'unixepoch', 'localtime') AS time,
+                   player_a,
+                   '(' || a_hand || ')',
+                   '(' || b_hand || ')',
+                   player_b,
+                   winner
+              FROM games
+             ORDER BY 2 DESC """
     )
 
-    print(data1)
-    print(data2)
+    g = [
+        ['{0:<25} {1:<25} <strong>{2:<18} {3:>10}</strong> vs. {4:<11} {5:<}'.format(x[0], x[1], x[2], x[3], x[4], x[5])] if x[2] == x[6] else
+        ['{0:<25} {1:<25} {2:<18} {3:>10} vs. <strong>{4:<11} {5:<}</strong>'.format(x[0], x[1], x[2], x[3], x[4], x[5])] if x[5] == x[6] else
+        ['{0:<25} {1:<25} {2:<18} {3:>10} vs. {4:<11} {5:<}'.format(x[0], x[1], x[2], x[3], x[4], x[5])] for x in games
+    ]
 
-    data["most_games"] = data1[0][0]
-    data["most_wins"] = data1[0][1]
-    data["best_ratio"] = data1[0][2]
-    data["mph"] = data1[0][3]
-    data["mvh"] =  data1[0][4]
-    data["least_games"] = data2[0][0]
-    data["most_losses"] = data2[0][1]
-    data["worst_ratio"] = data2[0][2]
-    data["lph"] = data2[0][3]
-    data["mdh"] =  data2[0][4]
+    items_per_page = 200
+    ret_items = paginate(g, items_per_page, page)
 
-    return render_template('games.html', data=data)
+    data["most_games"] = stats[0][0]
+    data["most_wins"] = stats[0][1]
+    data["best_ratio"] = stats[0][2]
+    data["mph"] = stats[0][3]
+    data["mvh"] =  stats[0][4]
+    return render_template('games.html', data=data, games=ret_items)
 
 
 
